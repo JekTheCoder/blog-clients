f60e72e7a02efc22f1d7e4fe029d405a3ddc8d58
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2023-10-31 21:58:57 -0500

    refactor(ui): export button styles from scss files

--- apps/admin/src/routes/storybook/+page.svelte
+++ apps/admin/src/routes/storybook/+page.svelte
@@ -1,16 +1,51 @@
 <script>
 	import { FlashingDots } from 'ui/dots';
 	import { OutlineFormField } from 'ui/form-field';
-	import { MatButton } from 'ui/button';
+	import { Icon } from 'ui/icon';
 </script>
 
 <main class="container mx-auto mt-4 grid gap-4">
-	<div class="flex gap-x-4">
-		<button class="button primary raised">Foo</button>
-		<button class="button primary outlined">Foo</button>
-		<button class="button accent raised">Foo</button>
-		<button class="button accent outlined">Foo</button>
+	<article>
+		<h2>Button</h2>
 
+		<section class="grid grid-cols-4 gap-4">
+			<button class="button primary raised">Foo</button>
+			<button class="button primary outlined">Foo</button>
+			<button class="button accent raised">Foo</button>
+			<button class="button accent outlined">Foo</button>
+
+			<button class="button primary basic">Foo</button>
+			<button class="button primary icon">
+				<Icon icon="ic:baseline-home" />
+			</button>
+			<button class="button accent basic">Foo</button>
+			<button class="button accent icon">
+				<Icon icon="ic:baseline-home" />
+			</button>
+		</section>
+
+		<section class="grid grid-cols-4 gap-x-4">
+			<button class="button primary raised" disabled>Foo</button>
+			<button class="button primary outlined" disabled>Foo</button>
+			<button class="button accent raised" disabled>Foo</button>
+			<button class="button accent outlined" disabled>Foo</button>
+
+			<button class="button primary basic" disabled>Foo</button>
+			<button class="button primary icon" disabled>
+				<Icon icon="ic:baseline-home" />
+			</button>
+			<button class="button accent basic" disabled>Foo</button>
+			<button class="button accent icon" disabled>
+				<Icon icon="ic:baseline-home" />
+			</button>
+		</section>
+
+		<section>
+
+		</section>
+	</article>
+
+	<div class="flex gap-x-4">
 		<div class="flex gap-x-4 items-center">
 			<FlashingDots class="primary" />
 			<FlashingDots class="accent" />
@@ -18,10 +53,6 @@
 	</div>
 
 	<div class="flex gap-x-4">
-		<MatButton>
-
-		</MatButton>
-
 		<button class="button primary raised"
 			>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea officiis aliquam odio
 			consequuntur distinctio dolore omnis quo necessitatibus! Neque, doloribus?</button
