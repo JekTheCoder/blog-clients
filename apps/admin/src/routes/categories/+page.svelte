922fd049006c923c0815b8149e1fdd6f26c79a2f
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2023-11-05 17:01:52 -0500

    feat(admin): can create categories

--- apps/admin/src/routes/categories/+page.svelte
+++ apps/admin/src/routes/categories/+page.svelte
@@ -1,12 +1,47 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { OutlineFormField } from 'ui/form-field';
+	import { create } from 'backend/categories';
+	import type { EventHandler } from 'svelte/elements';
+	import { writable } from 'svelte/store';
 
 	export let data: PageData;
+
+	type CategoryCreation = {
+		key: number;
+		status: Promise<unknown>;
+		data: {
+			name: string;
+		};
+	};
+
+	const creationCategories = writable<CategoryCreation[]>([]);
+
+	const handleSubmit: EventHandler<SubmitEvent, HTMLFormElement> = ({ currentTarget: form }) => {
+		const formData = new FormData(form);
+		const name = formData.get('name');
+		if (!name) return;
+
+		const createReq = create({
+			name: name.toString()
+		});
+
+		creationCategories.update((categories) => {
+			categories.push({
+				key: Math.random(),
+				status: createReq,
+				data: {
+					name: name.toString()
+				}
+			});
+
+			return categories;
+		});
+	};
 </script>
 
 <main class="container mx-auto">
-	<form class="grid gap-4" on:submit|preventDefault>
+	<form class="grid gap-4" on:submit|preventDefault={handleSubmit}>
 		<OutlineFormField>
 			<label for="">Name</label>
 			<input type="text" required name="name" />
@@ -18,6 +53,22 @@
 	</form>
 
 	<ul>
+		{#each $creationCategories as category (category.key)}
+			<li>
+				<span>
+					{category.data.name}
+				</span>
+
+				<span>
+					{#await category.status}
+						...
+					{:then result}
+						{result}
+					{/await}
+				</span>
+			</li>
+		{/each}
+
 		{#each data.categories as category}
 			<li>
 				{category}
