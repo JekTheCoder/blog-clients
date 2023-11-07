44a17ceae22bc1340ca6d9bc90a12d88ce9a424f
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2023-11-06 23:02:24 -0500

    chore(backend): define categories' type defs

--- packages/backend/src/routes/categories/index.ts
+++ packages/backend/src/routes/categories/index.ts
@@ -1,19 +1,21 @@
 import axios from 'axios';
 import { apiUrl } from '../../config';
 import { authClient } from '../../client';
+import { IdReturn } from '../../models/id-return';
 
 export type Category = {
-
-}
+  id: string;
+  name: string;
+};
 
 export function getAll() {
-  return axios.get<Category[]>(apiUrl+'/categories');
+  return axios.get<Category[]>(apiUrl + '/categories');
 }
 
 export type CategoryRequest = {
-	name: string;
-}
+  name: string;
+};
 
 export function create(category: CategoryRequest) {
-	return authClient.post(apiUrl+'/categories', category);
+  return authClient.post<IdReturn>(apiUrl + '/categories', category);
 }
