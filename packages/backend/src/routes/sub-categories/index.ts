b9ed2577811e49f92296c5b0dd3f132af2b62ec0
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2023-11-07 20:34:23 -0500

    feat(backend): register api endpoints for sub-categories & tags

---
+++ packages/backend/src/routes/sub-categories/index.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { apiUrl } from '../../config';
+
+export type SubCategory = {
+  id: string;
+  name: string;
+};
+
+export function getAll(categoryId: string) {
+  return axios.get<SubCategory[]>(`${apiUrl}/categories/${categoryId}/sub-categories/`);
+}
+
+export type SubCategoryRequest = {
+  name: string;
+};
+
+export function createOne(categoryId: string, payload: SubCategoryRequest) {
+  return axios.post(`${apiUrl}/categories/${categoryId}/sub-categories/`, payload);
+}
---
+++ packages/backend/src/routes/tags/index.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { apiUrl } from '../../config';
+
+export type Tag = {
+  id: string;
+  name: string;
+	color: string
+};
+
+export function getAll(categoryId: string) {
+  return axios.get<Tag[]>(`${apiUrl}/categories/${categoryId}/tags/`);
+}
+
+export type TagRequest = {
+  name: string;
+};
+
+export function createOne(categoryId: string, payload: TagRequest) {
+  return axios.post(`${apiUrl}/categories/${categoryId}/tags/`, payload);
+}
