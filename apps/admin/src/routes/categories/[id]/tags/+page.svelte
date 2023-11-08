58fd44827dcfa346a5ccbe8b4ab94588045b4b5b
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2023-11-07 21:31:00 -0500

    feat(admin): can list tags

--- apps/admin/src/routes/categories/[id]/tags/+page.svelte
+++ apps/admin/src/routes/categories/[id]/tags/+page.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { OutlineFormField } from 'ui/form-field';
-	import { create } from 'backend/categories';
+	import { createOne, type TagRequest } from 'backend/tags';
 	import type { EventHandler } from 'svelte/elements';
 	import { writable } from 'svelte/store';
+	import { page } from '$app/stores';
 
 	export let data: PageData;
+	let id = $page.params.id;
 
 	type CategoryCreation = {
 		key: number;
 		status: Promise<unknown>;
-		data: {
-			name: string;
-		};
+		data: TagRequest;
 	};
 
 	const creationCategories = writable<CategoryCreation[]>([]);
@@ -20,19 +20,20 @@
 	const handleSubmit: EventHandler<SubmitEvent, HTMLFormElement> = ({ currentTarget: form }) => {
 		const formData = new FormData(form);
 		const name = formData.get('name');
-		if (!name) return;
+		const color = formData.get('color');
+		if (!name || !color) return;
 
-		const createReq = create({
-			name: name.toString()
-		});
+		const req = {
+			name: name.toString(),
+			color: color.toString()
+		};
+		const createReq = createOne(id, req);
 
 		creationCategories.update((categories) => {
 			categories.push({
 				key: Math.random(),
 				status: createReq,
-				data: {
-					name: name.toString()
-				}
+				data: req
 			});
 
 			return categories;
@@ -46,10 +47,17 @@
 	<h1>Tags</h1>
 
 	<form class="grid gap-4" on:submit|preventDefault={handleSubmit}>
-		<OutlineFormField>
-			<label for="">Name</label>
-			<input type="text" required name="name" />
-		</OutlineFormField>
+		<div class="row gap-4">
+			<OutlineFormField>
+				<label for="">Name</label>
+				<input type="text" required name="name" />
+			</OutlineFormField>
+
+			<div class="grid gap-2">
+				<label for="">Color</label>
+				<input type="color" name="color" required />
+			</div>
+		</div>
 
 		<div class="flex gap-x-2 justify-end">
 			<button type="submit" class="button primary raised">Submit</button>
@@ -63,10 +71,17 @@
 			</li>
 		{/each}
 
-		<!-- {#each data.categories as category (category.id)} -->
-		<!-- 	<li class="flex justify-between"> -->
-		<!-- 		{category.name} -->
-		<!-- 	</li> -->
-		<!-- {/each} -->
+		{#each data.tags as tag (tag.id)}
+			<li class="flex justify-between">
+				{tag.name}
+			</li>
+		{/each}
 	</ul>
 </main>
+
+<style>
+	.row {
+		display: grid;
+		grid-template-columns: 3fr 1fr;
+	}
+</style>
---
+++ apps/admin/src/routes/categories/[id]/tags/+page.ts
@@ -0,0 +1,11 @@
+import type { PageLoad } from './$types';
+import { getAll } from 'backend/tags';
+
+export const load: PageLoad = async ({ params }) => {
+	const { id } = params;
+	const tags = await getAll(id);
+
+	return {
+		tags: tags.data
+	};
+};
