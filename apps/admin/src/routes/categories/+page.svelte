56fd1293786745d82ffa3df498d890ea9d2cfea9
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2023-11-27 20:39:45 -0500

    feat | can delete categories

--- apps/admin/src/routes/categories/+page.svelte
+++ apps/admin/src/routes/categories/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { OutlineFormField } from 'ui/form-field';
-	import { create } from 'backend/categories';
+	import { create, type Category, deleteOne } from 'backend/categories';
 	import type { EventHandler } from 'svelte/elements';
 	import { writable } from 'svelte/store';
 	import CategoryPromise from '$lib/components/category/CategoryPromise.svelte';
+	import { Icon } from 'ui/icon';
 
 	export let data: PageData;
 
@@ -41,6 +42,10 @@
 
 		form.reset();
 	};
+
+	const spawnDelete = ({ id }: Category) => {
+		deleteOne(id);
+	};
 </script>
 
 <main class="container mx-auto grid gap-4">
@@ -71,6 +76,16 @@
 				</span>
 
 				<div class="flex gap-x-2">
+					<button class="button icon accent">
+						<Icon icon="material-symbols:edit" />
+					</button>
+
+					<button class="button icon warn" on:click={() => spawnDelete(category)}>
+						<Icon icon="material-symbols:delete" />
+					</button>
+
+					|
+
 					<a href="categories/{category.id}/sub-categories" class="link"> SubCategories </a>
 					<a href="categories/{category.id}/tags" class="link"> Tags </a>
 				</div>
