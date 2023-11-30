5091b307d9528df108afbef4e4ffa1cbff282d6f
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2023-11-29 21:25:30 -0500

    feat(blog-frame): accept custom classes

--- packages/blog-frame/src/BlogFrame.svelte
+++ packages/blog-frame/src/BlogFrame.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-	import './_blog_frame.scss';
-	export let content: string;
+  import './_blog_frame.scss';
+  export let content: string;
+
+  let klass = '';
+  export { klass as class };
 </script>
 
-<article class="blog-frame">
-	{@html content} 
+<article class={'blog-frame' + ' ' + klass}>
+  {@html content}
 </article>
-
