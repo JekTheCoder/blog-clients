236556599843af268c548f3557f0ec51263028e4
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2023-11-29 21:06:03 -0500

    feat(admin): list sub-categories & tags on category select

--- apps/admin/src/routes/blogs/new/+page.svelte
+++ apps/admin/src/routes/blogs/new/+page.svelte
@@ -5,8 +5,15 @@
 	import { BlogFrame } from 'blog-frame';
 	import type { MessageResult, WRequest } from './type';
 	import { blog } from './blog';
-	import { getAll } from 'backend/categories';
+	import type { PageData } from './$types';
+	import { OutlineFormField } from 'ui/form-field';
+	import type { EventHandler } from 'svelte/elements';
+	import { writable } from 'svelte/store';
+	import { getAll as getAllSubCategories, type SubCategory } from 'backend/sub-categories';
+	import DropdownSearch from '$lib/backend/ui/search/DropdownSearch.svelte';
+	import { getAll as getAllTags, type Tag } from 'backend/tags';
 
+	export let data: PageData;
 	let content = blog;
 
 	let ws: WebSocket;
@@ -45,14 +52,65 @@
 		ws.send(JSON.stringify(req));
 	};
 
-	const categories = getAll().then(console.log);
+	const categorySelected = writable(data.categories[0]?.id ?? null);
+
+	let subCategories: SubCategory[] = [];
+	$: {
+		if ($categorySelected) {
+			subCategories.length = 0;
+			getAllSubCategories($categorySelected).then((res) => {
+				subCategories = res.data;
+			});
+		}
+	}
+
+	let tags: Tag[] = [];
+	$: {
+		if ($categorySelected) {
+			tags.length = 0;
+			getAllTags($categorySelected).then((res) => {
+				tags = res.data;
+			});
+		}
+	}
+
+
+	const onSubmit: EventHandler<SubmitEvent, HTMLFormElement> = (e) => {};
 </script>
+
 <section />
 
-<main class="container mx-auto">
+<main class="container mx-auto grid gap-4">
 	<BlogFrame {content} />
-</main>
 
-<footer>
-	<button class="button primary raised" on:click={handleSave}>Save</button>
-</footer>
+	<hr />
+
+	<form action="" class="grid grid-cols-3 gap-x-4" on:submit|preventDefault={onSubmit}>
+		<OutlineFormField>
+			<select name="categoryId" on:change={(e) => categorySelected.set(e.target?.value ?? null)}>
+				{#each data.categories as category (category.id)}
+					<option value={category.id}>{category.name}</option>
+				{/each}
+			</select>
+		</OutlineFormField>
+
+		<div>
+			<OutlineFormField>
+				<DropdownSearch items={subCategories} />
+			</OutlineFormField>
+			<ul />
+		</div>
+
+		<div>
+			<OutlineFormField>
+				<DropdownSearch items={tags} />
+			</OutlineFormField>
+
+			<ul />
+		</div>
+	</form>
+
+	<footer class="flex justify-end">
+		<button class="button primary raised" on:click={handleSave}>Save</button>
+	</footer>
+</main>
