0962e2887e9e954a5108cfc72882f7ef13208587
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2023-12-17 11:22:29 -0500

    feat(backend): register upload-images endpoint

--- packages/backend/src/routes/blogs/index.ts
+++ packages/backend/src/routes/blogs/index.ts
@@ -11,3 +11,13 @@ export type BlogRequest = {
 export function createOne(payload: BlogRequest) {
   return authClient.post<IdReturn>(apiUrl + '/blogs', payload);
 }
+
+export function uploadImages(blogId: string, files: File[]) {
+  const formData = new FormData();
+
+  for (const file of files) {
+    formData.append('image', file);
+  }
+
+  return authClient.post<void>(`${apiUrl}/blogs/${blogId}/public/`, files);
+}
