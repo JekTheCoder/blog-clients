fe7ae1a53026930ba6e5556a58638591c6c72099
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2023-12-30 18:37:17 -0500

    feat(auth): expose login & register error

--- packages/auth/src/features/login.ts
+++ packages/auth/src/features/login.ts
@@ -2,8 +2,6 @@ import { login as httpLogin } from '../backend';
 import { signIn } from './sign-in';
 
 export async function login(username: string, password: string) {
-	try {
-		const { user, refreshToken, token } = await httpLogin(username, password);
-		signIn(user, token, refreshToken);
-	} catch (_) {}
+	const { user, refreshToken, token } = await httpLogin(username, password);
+	signIn(user, token, refreshToken);
 }
--- packages/auth/src/features/register.ts
+++ packages/auth/src/features/register.ts
@@ -1,13 +1,7 @@
-import {
-	register as registerHttp,
-	type RegisterResponse,
-	RegisterRequest,
-} from '../backend';
+import { register as registerHttp, RegisterRequest } from '../backend';
 import { signIn } from './sign-in';
 
 export async function register(request: RegisterRequest) {
-	try {
-		const { data } = await registerHttp(request);
-		signIn(data.user, data.token, data.refreshToken);
-	} catch (_) {}
+	const { data } = await registerHttp(request);
+	signIn(data.user, data.token, data.refreshToken);
 }
