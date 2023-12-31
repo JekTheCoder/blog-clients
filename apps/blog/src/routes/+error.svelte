c2b88fd8672c38af1c28846109b13c07afbfb560
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2023-12-31 15:28:25 -0500

    feat(blog): setup custom error page

---
+++ apps/blog/src/routes/+error.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<h1>Oh! An error occurred</h1>
+
+<p>
+	An error ocurred with status <strong>{$page.status}</strong>. The server responded the following
+	message:
+</p>
+
+<p>
+	{$page.error?.message}
+</p>
+
+<p>Please, contact the server administrator</p>
