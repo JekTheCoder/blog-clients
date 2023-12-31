ef86039a258f6a904b4cacd1e581ba8d9778af43
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2023-12-31 15:49:57 -0500

    build: try fix package svelte/compiler not found

--- apps/blog/src/routes/+error.svelte
+++ apps/blog/src/routes/+error.svelte
@@ -2,15 +2,21 @@
 	import { page } from '$app/stores';
 </script>
 
-<h1>Oh! An error occurred</h1>
+<header>
+	<h1>Oh! An error occurred</h1>
+</header>
 
-<p>
-	An error ocurred with status <strong>{$page.status}</strong>. The server responded the following
-	message:
-</p>
+<main>
+	<p>
+		An error ocurred with status <strong>{$page.status}</strong>. The server responded the following
+		message:
+	</p>
 
-<p>
-	{$page.error?.message}
-</p>
+	<p>
+		{$page.error?.message}
+	</p>
 
-<p>Please, contact the server administrator</p>
+	<p>Please, contact the server administrator</p>
+
+	<a href="/">Go to home</a>
+</main>
