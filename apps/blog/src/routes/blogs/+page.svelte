8fc03b88f164315fb4bf595c0de8f3fe5e4d3933
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-01-14 21:11:42 -0500

    feat(blogs): add cooming soon settings dialog on blogs search

--- apps/blog/src/routes/blogs/+page.svelte
+++ apps/blog/src/routes/blogs/+page.svelte
@@ -8,9 +8,11 @@
 	import { goto } from '$app/navigation';
 	import { OutlineFormField } from 'ui/form-field';
 	import type { FormEventHandler } from 'svelte/elements';
+	import { Dialog } from 'ui/dialog';
 
 	export let data: PageData;
 	let searchParams = new URLSearchParams();
+	let settingsDialog: Dialog;
 
 	$: page = Number($svPage.url.searchParams.get('page')) ?? 0;
 
@@ -28,10 +30,13 @@
 		if (timeout) clearTimeout(timeout);
 
 		const search = event.currentTarget.value;
-		timeout = setTimeout(() => {
-			searchParams.set('search', search);
+		timeout = setTimeout(() => { searchParams.set('search', search);
 			updateQuery(searchParams);
 		}, 300);
+	};
+
+	const openSettings = () => {
+		settingsDialog.open();
 	};
 </script>
 
@@ -40,12 +45,18 @@
 
 	<div class="grid gap-4">
 		<section>
-			<search>
-				<OutlineFormField>
-					<svelte:fragment slot="label">Search</svelte:fragment>
-					<input type="text" on:input={searchHandler} />
-				</OutlineFormField>
-			</search>
+			<div class="grid grid-cols-[1fr_auto] gap-x-2">
+				<search>
+					<OutlineFormField>
+						<svelte:fragment slot="label">Search</svelte:fragment>
+						<input type="text" on:input={searchHandler} />
+					</OutlineFormField>
+				</search>
+
+				<button class="button icon self-end" on:click={openSettings}>
+					<Icon icon="material-symbols:settings" />
+				</button>
+			</div>
 		</section>
 
 		<div class="blogs gap-y-8 gap-x-4">
@@ -55,7 +66,7 @@
 		</div>
 	</div>
 
-	<footer class="container mx-auto my-8" slot="footer">
+	<footer class="container mx-auto my-8 stepper" slot="footer">
 		<button class="button icon" on:click={() => updatePage(page - 1)} disabled={page === 0}>
 			<Icon icon="ooui:next-rtl" />
 		</button>
@@ -66,13 +77,23 @@
 	</footer>
 </PageLayout>
 
-<style>
+<Dialog bind:this={settingsDialog}>
+	<header class="dialog-header">
+		<p class="text-center dialog-title">Soon!</p>
+	</header>
+
+	<footer class="dialog-footer flex justify-end">
+		<button class="button basic" on:click={settingsDialog.close}>Close</button>
+	</footer>
+</Dialog>
+
+<style lang="scss">
 	.blogs {
 		display: grid;
 		grid-template-columns: repeat(auto-fill, minmax(350px, max-content));
 	}
 
-	footer {
+	.stepper {
 		display: flex;
 		justify-content: space-between;
 	}
