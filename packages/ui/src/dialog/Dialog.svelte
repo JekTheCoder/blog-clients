e81c0a354e1bf3627e7a207db5ec3774687bf7c0
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-01-14 21:11:15 -0500

    feat(ui): create Dialog utils

---
+++ packages/ui/src/dialog/Dialog.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { MouseEventHandler } from 'svelte/elements';
+	let dialog: HTMLDialogElement;
+
+	export const open = () => {
+		dialog.showModal();
+	};
+
+	export const close = () => {
+		dialog.close();
+	};
+
+	const onDialogClick: MouseEventHandler<HTMLDialogElement> = event => {
+		if (event.target !== dialog) return;
+		dialog.close();
+	};
+</script>
+
+<dialog class="dialog" bind:this={dialog} on:click={onDialogClick}>
+	<div>
+		<slot {close} />
+	</div>
+</dialog>
