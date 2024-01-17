c7d8a699e834bb5b44f974d5cc3e95b2b686d3a0
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-01-16 21:04:47 -0500

    build(blogs): import tailwind config using vite

--- apps/blog/vite.config.ts
+++ apps/blog/vite.config.ts
@@ -1,6 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import { join } from 'node:path';
+import tailwindcss from 'tailwindcss';
+
+const tailwindConfig = tailwindcss({
+	config: '../../packages/ui/tailwind.config.js'
+});
 
 export default defineConfig({
 	server: {
@@ -9,6 +14,11 @@ export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	css: {
+		postcss: {
+			plugins: [tailwindConfig]
+		}
 	},
 	resolve: {
 		alias: [
