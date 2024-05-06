8c8e16c4ed9d92e3c7e978bbf581c3c319cdeb79
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-05-06 16:40:32 -0500

    feat(blog-astro): create page for blogs content

--- apps/blog-astro/astro.config.js
+++ apps/blog-astro/astro.config.js
@@ -7,6 +7,7 @@ import { join } from 'node:path';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [svelte(), tailwind()],
+	output: 'hybrid',
 	vite: {
 		resolve: {
 			alias: [
