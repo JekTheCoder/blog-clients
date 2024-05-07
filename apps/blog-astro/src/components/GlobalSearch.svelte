58c442b505ed5f52521dcb4bf82a2061c6eddebe
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-05-06 22:09:32 -0500

    feat(blog-astro): can search by global form

---
+++ apps/blog-astro/src/components/GlobalSearch.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { OutlineFormField } from 'ui/form-field';
+	let klass = '';
+
+	export { klass as class };
+</script>
+
+<form action="/blogs" class={klass}>
+	<OutlineFormField class="w-full">
+		<input type="text" name="search" placeholder="Search a blog" />
+	</OutlineFormField>
+</form>
